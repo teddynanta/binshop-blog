c85baff460eacc53c623def3760120fbb878202d
teddynanta <teddy.nanta99@gmail.com>
2022-10-25 13:13:02 +0700

    first

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canManageBinshopsBlogPosts()
+    {
+        // Enter the logic needed for your app.
+        // Maybe you can just hardcode in a user id that you
+        //   know is always an admin ID?
+
+        if ($this->email === "admin@gmail.com") {
+
+            // return true so this user CAN edit/post/delete
+            // blog posts (and post any HTML/JS)
+
+            return true;
+        }
+    }
 }
--- database/migrations/2020_10_22_132005_create_binshops_configurations_table.php
+++ database/migrations/2020_10_22_132005_create_binshops_configurations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateBinshopsConfigurationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('binshops_configurations', function (Blueprint $table) {
+            $table->string("key")->primary();
+            $table->string("value");
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('binshops_configurations');
     }
-};
+}
